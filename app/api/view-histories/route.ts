4bf7bb296e8ebb1b8cead6d45eee556e0d02e2ee
daffakbar13 <daffa.akbar@sociolla.com>
2025-01-05 07:52:03 +0700

    fix: view histories

--- app/api/view-histories/route.ts
+++ app/api/view-histories/route.ts
@@ -7,13 +7,12 @@ import ViewHistoriesModel from '@/server/models/view_histories'
 export async function GET() {
   const viewHistories = await ViewHistoriesModel()
   const groups = await GuestGroupsModel()
-  const groupList = await groups.find({}).toArray()
-  const data = await viewHistories
-    .find({}, { sort: ['created_at', -1] })
-    .toArray()
-    .then((res) =>
-      res.map((e) => ({ ...e, group_name: groupList.find((g) => g._id.equals(e.group_id))?.name })),
-    )
+  const data = await viewHistories.find({}, { sort: ['created_at', -1] }).toArray()
+  const groupList = await groups.find({ _id: { $in: data.map((d) => d.group_id) } }).toArray()
+  console.log({ data, groupList })
+  data.forEach((row) => {
+    row.group_name = groupList.find((g) => g._id.equals(row.group_id))?.name
+  })
 
   return new NextResponse(JSON.stringify({ data }), { status: 200 })
 }
